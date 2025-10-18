3783246e00b134978183a06f1d75e5c5681a41dc
Hamdan <hamdanahmadkhawaja@gmail.com>
2025-10-18 14:23:09 +0500

    practiced css, forms,render template

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',name="Hamdan")
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username,password)
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
